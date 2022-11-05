275677827379bf9ff34281ed2ca259c52f0e2f57
phuoc <duccp20@gmail.com>
2022-11-06 00:26:31 +0700

    done

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// element toggle function
+const elementToggleFunc = function (elem) {
+  elem.classList.toggle("active");
+};
+
+// sidebar variables
+const sidebar = document.querySelector("[data-sidebar]");
+const sidebarBtn = document.querySelector("[data-sidebar-btn]");
+
+// sidebar toggle functionality for mobile
+sidebarBtn.addEventListener("click", function () {
+  elementToggleFunc(sidebar);
+});
+
+//play song
+const song = document.querySelector("#song").play();
